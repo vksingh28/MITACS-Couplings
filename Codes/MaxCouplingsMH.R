01c6eb95ef07aa6ba2ffc720a17fbd05ac845988
vksingh <vksingh.iitk28@gmail.com>
2022-05-17 14:02:55 -0400

    code: minor edits and add comments

--- Codes/MaxCouplingsMH.R
+++ Codes/MaxCouplingsMH.R
@@ -24,10 +24,13 @@ Pi_X.Y = function(x, y) {
 }
 
 # Function to draw from r_X(.)
+# Rejection sampling using standard cauchy distribution
 Remainder_X = function(x, y){
+	# unnormalised pdf of r_X(.)
 	r_X = function(t){
 		dnorm(t, mean = x, sd = 1) - min(dnorm(t, mean = x, sd = 1), dnorm(t, mean = y, sd = 1))
 	}
+	# interval used to calculate max of f/g
 	interval = c(min(x, y)-10, max(x, y)+10)
 	M = max(r_X(interval)/dcauchy(interval))
 	accept = 0
@@ -44,10 +47,12 @@ Remainder_X = function(x, y){
 
 # Function to draw from r_Y(.)
 Remainder_Y = function(x, y){
-	interval = c(min(x, y)-10, max(x, y)+10)
+	# unnormalised pdf of r_X(.)
 	r_Y = function(t){
 		dnorm(t, mean = y, sd = 1) - min(dnorm(t, mean = x, sd = 1), dnorm(t, mean = y, sd = 1))
 	}
+	# interval used to calculate max of f/g
+	interval = c(min(x, y)-10, max(x, y)+10)
 	M = max(r_Y(interval)/dcauchy(interval))
 	accept = 0
 	while(accept == 0){
@@ -77,13 +82,13 @@ X = numeric(N)
 Y = numeric(N)
 
 # Initial draws for both the chains
-X[1] = rnorm(1, 0, 1)
-Y[1] = rnorm(1, 0, 1)
+X[1] = rnorm(1)
+Y[1] = rnorm(1)
 
 # Unif r.v.s
 U = runif(N, 0, 1)
 
-i = 2
+# Final algorithm
 for(i in 2:N){
 	if(U[i] < Normalize_Pi(X[i-1], Y[i-1])$value){
 		X[i] = Y[i] = Pi_X.Y(X[i-1], Y[i-1])
