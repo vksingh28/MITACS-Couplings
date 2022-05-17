55cc9c1c9b28ba2c07c75bff561161f622ffa09e
vksingh <vksingh.iitk28@gmail.com>
2022-05-17 14:00:25 -0400

    code: complete all the remaining function.

--- Codes/MaxCouplingsMH.R
+++ Codes/MaxCouplingsMH.R
@@ -1,16 +1,64 @@
+set.seed(1)
+
 # Function to draw from \Pi_{X, Y}(.)
+# Rejection sampling using standard cauchy distribution
 Pi_X.Y = function(x, y) {
+	# unnormalised pdf of Pi_{X, Y}
+	Pi = function(t){
+		min(dnorm(t, mean = x, sd = 1), dnorm(t, mean = y, sd = 1))
+	}
 
+	# interval to calculate max of f/g
+	interval = c(min(x, y)-10, max(x, y)+10)
+	M = max(Pi(interval)/dcauchy(interval))
+	accept = 0
+	while(accept == 0){
+		U = runif(1)
+		prop = rcauchy(1)
+		ratio = Pi(prop)/(M*dcauchy(prop))
+		if(U <= ratio){
+			accept = 1
+			return(prop)
+		}
+	}
 }
 
-# Function to draw from \r_X(.)
+# Function to draw from r_X(.)
 Remainder_X = function(x, y){
-
+	r_X = function(t){
+		dnorm(t, mean = x, sd = 1) - min(dnorm(t, mean = x, sd = 1), dnorm(t, mean = y, sd = 1))
+	}
+	interval = c(min(x, y)-10, max(x, y)+10)
+	M = max(r_X(interval)/dcauchy(interval))
+	accept = 0
+	while(accept == 0){
+		U = runif(1)
+		prop = rcauchy(1)
+		ratio = r_X(prop)/(M*dcauchy(prop))
+		if(U <= ratio){
+			accept = 1
+			return(prop)
+		}
+	}
 }
 
-# Function to draw from \r_Y(.)
+# Function to draw from r_Y(.)
 Remainder_Y = function(x, y){
-
+	interval = c(min(x, y)-10, max(x, y)+10)
+	r_Y = function(t){
+		dnorm(t, mean = y, sd = 1) - min(dnorm(t, mean = x, sd = 1), dnorm(t, mean = y, sd = 1))
+	}
+	M = max(r_Y(interval)/dcauchy(interval))
+	accept = 0
+	while(accept == 0){
+		U = runif(1)
+		prop = rcauchy(1)
+		ratio = r_Y(prop)/(M*dcauchy(prop))
+		if(U <= ratio){
+			accept = 1
+			return(prop)
+		}
+	}
 }
 
 # Function that returns normalizing constant of \Pi_{X, Y}(.)
@@ -22,7 +70,7 @@ Normalize_Pi = function(x, y){
 }
 
 # Number of MC draws
-N = 1e4
+N = 1e2
 
 # Parallel MCs
 X = numeric(N)
@@ -35,11 +83,13 @@ Y[1] = rnorm(1, 0, 1)
 # Unif r.v.s
 U = runif(N, 0, 1)
 
+i = 2
 for(i in 2:N){
-	if(U[i] < Normalize_Pi(X[i-1], Y[i-1])){
+	if(U[i] < Normalize_Pi(X[i-1], Y[i-1])$value){
 		X[i] = Y[i] = Pi_X.Y(X[i-1], Y[i-1])
 	} else {
 		X[i] = Remainder_X(X[i-1], Y[i-1])
 		Y[i] = Remainder_Y(X[i-1], Y[i-1])
 	}
 }
+print(head(X-Y, n = 20))
